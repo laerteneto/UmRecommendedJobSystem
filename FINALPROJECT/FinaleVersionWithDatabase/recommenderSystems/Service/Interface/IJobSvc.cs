9a585d042f37bd312a51f2c7321e57c330a2404f
larissacfs <lcfs@cin.ufpe.br>
2015-07-30 14:32:24 -0400

    New version with database

---
+++ FINALPROJECT/FinaleVersionWithDatabase/recommenderSystems/Service/Interface/IJobSvc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recommenderSystems.Service.Interface
+{
+    interface IJobSvc
+    {
+        string[] selectExpressionNames();
+        double[] selectExpressionDifficulty();
+    }
+}
---
+++ FINALPROJECT/FinaleVersionWithDatabase/recommenderSystems/Service/Plugin/JobSvcImpl.cs
@@ -0,0 +1,27 @@
+﻿using recommenderSystems.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recommenderSystems.Service.Plugin
+{
+    public class JobSvcImpl : IJobSvc
+    {
+        //jobs ids (used in expressions file)
+        public string[] selectExpressionNames()
+        {
+            JobService.ServiceWCFClient svc = new JobService.ServiceWCFClient();
+            Guid[] jobNames = svc.selectExpressionNames();
+            return Array.ConvertAll(jobNames, x => x.ToString());
+        }
+
+        //job difficulty (used in X)
+        public double[] selectExpressionDifficulty()
+        {
+            JobService.ServiceWCFClient svc = new JobService.ServiceWCFClient();
+            return svc.selectExpressionDifficulty();
+        }
+    }
+}
