ace71b31b0b213780d43c59e0aa00afeb6ad02e1
sadallo <sadallo@hotmail.com>
2015-08-05 11:33:03 -0400

    Business manager

---
+++ FINALPROJECT/FinaleVersionWithDatabaseV2/recommenderSystems/Business/BusinessManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using recommenderSystems.Service.Interface;
+using recommenderSystems.Service;
+
+namespace recommenderSystems.Service.Interface
+{
+    public abstract class BusinessManager
+    {
+        protected IService getService(String name)
+        {
+            return (Factory.getInstance()).getService(name);
+        }
+    }
+}
