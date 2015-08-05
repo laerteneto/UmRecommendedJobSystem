d855b3fcaea53cec9c83f004c4f77a35d11f1e62
sadallo <sadallo@hotmail.com>
2015-08-05 10:51:05 -0400

    recommenderV2

---
+++ FINALPROJECT/FinaleVersionWithDatabaseV2/recommenderSystems/Service/Interface/IRecruiteeSvc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recommenderSystems.Service.Interface
+{
+    interface IRecruiteeSvc
+    {
+        String[] selectRecruiteeNames();
+        double[] selectRecruiteeSkills();
+    }
+}
